af4acf06efccbba3ac4b47689e63de7e96543663
ViniciusU <vini.gama.15@gmail.com>
2023-09-02 16:46:20 -0300

    change hexfile

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include <nokia5110_LCD.h>
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
